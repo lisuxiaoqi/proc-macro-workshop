7bf7391e75016ff833ae9f625f19ec0a2575dcef
lisuxiaoqi <lisuxiaoqi@gmail.com>
2026-01-03 12:21:12 +0800

    debug 03 meta

--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -1,8 +1,8 @@
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{parse_macro_input, Data, DeriveInput, Fields};
+use syn::{parse_macro_input, Data, DeriveInput, Expr, ExprLit, Field, Fields, Lit, Meta};
 
-#[proc_macro_derive(CustomDebug)]
+#[proc_macro_derive(CustomDebug, attributes(debug))]
 pub fn derive(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
 
@@ -15,7 +15,7 @@ pub fn derive(input: TokenStream) -> TokenStream {
         _ => unimplemented!("Only DataStruct are supported!"),
     };
 
-    let f_names: Vec<_> = fields
+    let _f_names: Vec<_> = fields
         .iter()
         .map(|f| {
             let f_name = f.ident.as_ref().unwrap();
@@ -25,14 +25,41 @@ pub fn derive(input: TokenStream) -> TokenStream {
         })
         .collect();
 
+    let f_formats: Vec<_> = fields
+        .iter()
+        .map(|f| {
+            let f_name = f.ident.as_ref().unwrap();
+            let f_arg = parse_arg(f);
+            quote! {
+                .field(stringify!(#f_name), &format_args!(#f_arg, &self.#f_name))
+            }
+        })
+        .collect();
+
     quote! {
         impl std::fmt::Debug for #name{
             fn fmt(&self, f:&mut std::fmt::Formatter<'_>) -> std::fmt::Result{
                 f.debug_struct(stringify!(#name))
-                #(.field(stringify!(#f_names), &self.#f_names))*
+                #(#f_formats)*
                 .finish()
             }
         }
     }
     .into()
+}
+
+fn parse_arg(f: &Field) -> String {
+    let args = "{:?}";
+    for attr in &f.attrs {
+        if let Meta::NameValue(nv) = &attr.meta {
+            if nv.path.is_ident("debug") {
+                if let Expr::Lit(ExprLit { lit, .. }) = &nv.value {
+                    if let Lit::Str(lit_s) = &lit {
+                        return lit_s.value();
+                    }
+                }
+            }
+        }
+    }
+    args.into()
 }
