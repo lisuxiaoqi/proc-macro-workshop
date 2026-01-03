dce4469bd94014090788dd5ce9022baf5b4e946e
lisuxiaoqi <lisuxiaoqi@gmail.com>
2026-01-03 10:00:05 +0800

    debug 02

--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -1,8 +1,38 @@
 use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse_macro_input, Data, DeriveInput, Fields};
 
 #[proc_macro_derive(CustomDebug)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+    let input = parse_macro_input!(input as DeriveInput);
 
-    unimplemented!()
+    let name = &input.ident;
+    let fields = match &input.data {
+        Data::Struct(ds) => match &ds.fields {
+            Fields::Named(fs) => &fs.named,
+            _ => unimplemented!("Only Named Fields are supported"),
+        },
+        _ => unimplemented!("Only DataStruct are supported!"),
+    };
+
+    let f_names: Vec<_> = fields
+        .iter()
+        .map(|f| {
+            let f_name = f.ident.as_ref().unwrap();
+            quote! {
+                #f_name
+            }
+        })
+        .collect();
+
+    quote! {
+        impl std::fmt::Debug for #name{
+            fn fmt(&self, f:&mut std::fmt::Formatter<'_>) -> std::fmt::Result{
+                f.debug_struct(stringify!(#name))
+                #(.field(stringify!(#f_names), &self.#f_names))*
+                .finish()
+            }
+        }
+    }
+    .into()
 }
